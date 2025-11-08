0245717262db3468ee8f3f04fa3a343e8632df9f
박재현 <Park (PC Park-40)>
2025-11-08 13:11:31 +0900

    chore: UI 컴포넌트 및 i18n 설정 업데이트

--- src/components/GradientIcon.jsx
+++ src/components/GradientIcon.jsx
@@ -5,15 +5,13 @@ const GradientIcon = ({ icon, id }) => {
   const IconComponent = icon;
 
   return (
-    <svg width="3rem" height="3rem" viewBox="0 0 24 24">
+    <svg viewBox="0 0 24 24" style={{ width: '3rem', height: '3rem' }}>
       <defs>
-        {}
         <linearGradient id={id} x1="0%" y1="0%" x2="100%" y2="100%">
           <stop offset="0%" style={{ stopColor: '#b84182ff' }} />
           <stop offset="100%" style={{ stopColor: '#F8EBE4' }} />
         </linearGradient>
       </defs>
-      {}
       <IconComponent fill={`url(#${id})`} size="100%" />
     </svg>
   );
